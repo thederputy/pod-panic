ec06c04dc9c5eca7fd5572774c6831dedd59f4c0
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@a02642ab-ae55-3f25-f84e-0a2427a89e30>
2011-01-29 05:59:51 +0000

    - Added initial GameObject.cs to inherit from.

---
+++ trunk/PodPanic/PodPanic/GameObjects/GameObject.cs
@@ -0,0 +1,34 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+#endregion
+
+namespace PodPanic.GameObjects
+{
+    class GameObject
+    {
+        #region Attributes
+        public Texture2D sprite;
+        public Vector2 position;
+        public float velocity;
+        #endregion
+
+        public GameObject(Texture2D loadedTexture)
+        {
+            velocity = 0.0f;
+            position = Vector2.Zero;
+            sprite = loadedTexture;
+        }
+    }
+}
