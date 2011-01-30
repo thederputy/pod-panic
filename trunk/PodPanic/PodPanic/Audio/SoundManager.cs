6990b3f140e4ae0faf2e745e2dd89c4fd3b76409
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@a02642ab-ae55-3f25-f84e-0a2427a89e30>
2011-01-30 03:52:57 +0000

    Added SoundManager.cs

---
+++ trunk/PodPanic/PodPanic/Audio/SoundManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace PodPanic.Audio
+{
+    static class SoundManager
+    {
+        /// <summary>
+        /// Starts a sound. Used for ambient sounds
+        /// </summary>
+        /// <param name="sound">The sound to play</param>
+        public static void startLoopedSound(SoundEffectInstance sound, float volume)
+        {
+            if (sound.State == SoundState.Stopped)
+            {
+                sound.Volume = volume;
+                sound.IsLooped = true;
+                sound.Play();
+            }
+            else
+            {
+                sound.Resume();
+            }
+        }
+
+        /// <summary>
+        /// Plays a sound effect,(not a song)
+        /// </summary>
+        /// <param name="sound">The SoundEffectInstance to play</param>
+        /// <param name="volume">The volume of the sound</param>
+        public static void playSound(SoundEffectInstance sound, float volume)
+        {
+            if (sound.State == SoundState.Stopped)
+            {
+                sound.Volume = volume;
+                sound.Play();
+            }
+            else
+            {
+                sound.Resume();
+            }
+        }
+
+        /// <summary>
+        /// Pauses a sound effect
+        /// </summary>
+        /// <param name="sound">The SoundEffectInstance to pause</param>
+        public static void pauseSound(SoundEffectInstance sound)
+        {
+            if (sound.State == SoundState.Playing)
+                sound.Pause();
+        }
+
+        /// <summary>
+        /// Stops the sound effect.
+        /// </summary>
+        /// <param name="sound">The SoundEffectInstance to stop</param>
+        public static void stopSound(SoundEffectInstance sound)
+        {
+            if (sound.State == SoundState.Playing)
+                sound.Stop();
+        }
+    }
+}
