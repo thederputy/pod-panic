c0dd3a70c8bc9bfe7f1ca3d09afdc617104f1f7d
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@a02642ab-ae55-3f25-f84e-0a2427a89e30>
2011-03-18 22:14:13 +0000

    - Removed Xbox referenced and project. (temporarily, want to get the AppUp working first) - Added IntelAppup authorization to Program.cs - for some reason our GUID that we got from the submission of the app (get it from "My Dashboard" on the AppUp site) does not authorize, so the debug one is what is currently in use.

--- PodPanic/PodPanic/Program.cs
+++ PodPanic/PodPanic/Program.cs
@@ -9,6 +9,26 @@ namespace PodPanic
         /// </summary>
         static void Main(string[] args)
         {
+            //Authorization code for Intel AppUp(TM) software
+            com.intel.adp.AdpApplication app;
+            try
+            {
+                //TODO: Right click and select Get Application GUID to replace the debug GUID before submit it
+                app = new com.intel.adp.AdpApplication(new com.intel.adp.AdpApplicationId(0x11111111, 0x11111111, 0x11111111, 0x11111111)); //debug GUID
+                //app = new com.intel.adp.AdpApplication(new com.intel.adp.AdpApplicationId(0x07998DB6, 0x1FEA41B5, 0x9D6A8AE7, 0x56949529)); //Pod Panic GUID
+            }
+            catch (com.intel.adp.AdpException e)
+            {
+                if (e is com.intel.adp.AdpErrorException)
+                {
+                    // TO DO: add your logic to handle the errors during initialization
+                    System.Environment.Exit(1);
+                }
+                else if (e is com.intel.adp.AdpWarningException)
+                {
+                    // TO DO: add your logic to handle the warnings
+                }
+            }
             using (PodPanic game = new PodPanic())
             {
                 game.Run();
